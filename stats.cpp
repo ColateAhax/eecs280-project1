59657532787b8b866deb499d1ce5916be596e86a
ColateAhax <nico.socas@gmail.com>
2026-01-13 21:31:54 -0500

    Initial Commit

---
+++ stats.cpp
@@ -0,0 +1,57 @@
+// stats.cpp
+#include "stats.hpp"
+#include <cassert>
+#include <vector>
+#include <algorithm> // sort
+#include <cmath> // sqrt, modf
+
+using namespace std;
+
+int count(vector<double> v) {
+  assert(false);
+  return 0;
+}
+
+double sum(vector<double> v) {
+  assert(false);
+  return 0.0;
+}
+
+double mean(vector<double> v) {
+  assert(false);
+  return 0.0;
+}
+
+double median(vector<double> v) {
+  assert(false);
+  return 0.0;
+}
+
+double min(vector<double> v) {
+  assert(false);
+  return 0.0;
+}
+
+double max(vector<double> v) {
+  assert(false);
+  return 0.0;
+}
+
+double stdev(vector<double> v) {
+  assert(false);
+  return 0.0;
+}
+
+double percentile(vector<double> v, double p) {
+  assert(false);
+  return 0.0;
+}
+
+vector<double> filter(vector<double> v,
+                      vector<double> criteria,
+                      double target) {
+  assert(false);
+
+  std::vector<double> emptyObject;
+  return emptyObject;
+}
